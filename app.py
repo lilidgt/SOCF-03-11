c48469faca368624a04f4b2ff7a0dd429c87ba76
Lissa Deguti <lilideguti@gmail.com>
2025-11-14 03:21:36 +0000

    add studentsand /info route with json

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 APP = Flask(__name__)
+
+ESTUDANTES = "Lissa Deguti, Melissa Weiss e Fernanda Moraes"
 
 @APP.route("/")
 def index():
     return (
         "<h1>Projeto II – Sistemas Operacionais em Cloud</h1>"
-        "<p>Aplicação Flask inicial.</p>"
+        f"<p>Estudantes: {ESTUDANTES}</p>"
     )
+
+@APP.route("/info")
+def info():
+    return jsonify({"estudantes": ESTUDANTES})
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=5000, debug=True)
